9871d5e9f1378508219f1990dfdfd6fbb0e2e2ca
wassimbj <wassimbenjdida@gmail.com>
2021-01-31 15:19:22 +0100

    added select lang in navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,6 +3,14 @@ import React from 'react';
 import CakeImg from '../images/fw-icon.png'
 
 export default function NavBar() {
+  const selectedLang = typeof window !== 'undefined' ? window.localStorage.getItem('lang') : 'en';
+  const handleLangChange = (e) => {
+    if(typeof window !== 'undefined'){
+      window.localStorage.setItem('lang', e.target.value);
+      window.location.reload();
+    }
+  }
+
   return (
     <header className="bg-gray-800 px-3">
       <div className="text-center flex justify-around">
@@ -12,6 +20,10 @@ export default function NavBar() {
         <a href="https://github.com/wassimbj/sfw" target="_blank" className="block py-2 hover:text-gray-300">
           Github
         </a>
+        <select onChange={handleLangChange} className="text-gray-300 px-3 rounded-full bg-gray-800">
+          <option value="en" selected={selectedLang === 'en'}> English </option>
+          <option value="tn" selected={selectedLang === 'tn'}> Tounsiii </option>
+        </select>
       </div>
     </header>
   )
